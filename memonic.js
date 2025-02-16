a6238872a1aeae8c4885214aedd7eb5f05f83add
venkatesh2100 <venkyvenkat.iit@gmail.com>
2025-02-16 23:55:06 +0530

    reArragethModule

--- memonic.js
+++
@@ -1,9 +0,0 @@
-import * as bip39 from 'bip39'
-
-export const  message = bip39.generateMnemonic();
-//TODO: Try to Encrypt the Normal message to seeder 
-export const seed = bip39.mnemonicToSeedSync(message)
-console.log("Memonic:",message)
-// console.log("Seed message",seed);
-
-console.log("Seed Prase: ", seed.toString('hex'))
